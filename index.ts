1e3b13ef3b029f8dfd0f33b219f87f52c6dd27a8
matteo0003 <angoin.matteo@gmail.com>
2025-05-20 21:44:31 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,107 @@
+enum TokenType {
+    Alpha,
+    Number,
+
+    Equals,
+    BinaryOperator,
+
+    OpeningParenthesis,
+    ClosingParenthesis,
+
+    Let,
+    Const,
+}
+
+interface Token {
+    type: TokenType;
+    value: string;
+}
+
+function toToken(type: TokenType, value: string): Token {
+    return { type, value }
+}
+
+function isAlpha(value: string): boolean {
+    return /^[A-Z]$/i.test(value);
+}
+
+function isNumber(value: string): boolean {
+    return /^[0-9]$/.test(value);
+}
+
+const canBeSkippedValues: string[] = [" ", "\n", "\t"];
+
+function shouldBeSkipped(value: string): boolean {
+    // @ts-ignore
+    return canBeSkippedValues.includes(value);
+}
+
+const reservedKeywordsMap: { [key: string]: TokenType } = {
+    "let": TokenType.Let,
+    "const": TokenType.Const,
+};
+
+function tokenize(sourceCode: string): Token[] {
+    const tokens: Token[] = [];
+    const source: string[] = sourceCode.split("");
+
+    while (source.length > 0) {
+        console.log(source[0]);
+
+        switch (source[0]) {
+            case "=":
+                tokens.push(toToken(TokenType.Equals, source.shift()));
+                break;
+            case "*":
+            case "+":
+            case "-":
+            case "/":
+                tokens.push(toToken(TokenType.BinaryOperator, source.shift()));
+                break;
+            case "(":
+                tokens.push(toToken(TokenType.OpeningParenthesis, source.shift()));
+                break;
+            case ")":
+                tokens.push(toToken(TokenType.ClosingParenthesis, source.shift()));
+                break;
+            default:
+                if (isAlpha(source[0])) {
+                    let a: string = "";
+
+                    while (source.length > 0 && isAlpha(source[0])) {
+                        a += source.shift();
+                    }
+
+                    tokens.push(toToken(reservedKeywordsMap[a] ?? TokenType.Alpha, a));
+
+                    break;
+                }
+
+                if (isNumber(source[0])) {
+                    let n: string = "";
+
+                    while (source.length > 0 && isNumber(source[0])) {
+                        n += source.shift();
+                    }
+
+                    tokens.push(toToken(TokenType.Number, n));
+
+                    break;
+                }
+
+                if (shouldBeSkipped(source[0])) {
+                    source.shift();
+
+                    break;
+                }
+
+                console.error("Found something weird: " + source[0]);
+
+                // TODO: Exit process
+        }
+    }
+
+    return tokens;
+}
+
+console.log(tokenize("const x = 5"));
