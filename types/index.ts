fc07a5e69b68fbabfcb3e8aab46f968227075b18
matteo0003 <angoin.matteo@gmail.com>
2025-05-20 23:03:01 +0200

    feat(src): Added custom TokenError exception

---
+++ types/index.ts
@@ -0,0 +1,15 @@
+export enum TokenType {
+    Alpha,
+    Number,
+    Equals,
+    BinaryOperator,
+    OpeningParenthesis,
+    ClosingParenthesis,
+    Let,
+    Const,
+}
+
+export interface Token {
+    type: TokenType;
+    value: string;
+}
