4c317bcb5aba1942c8f823c8f064980146a9b884
FatySll <Fatoumatasall@esp.sn>
2025-05-27 16:52:02 +0200

    .

--- src/parser.ts
+++ src/parser.ts
@@ -404,12 +404,12 @@ export default class Parser {
     throw new InvalidTokenError("Expected ')' after condition");
   }
 
-  const trueCase = this.parseBlockStatement(); // ✅ bloc {}
+  const trueCase = this.parseBlockStatement();
 
   let falseCase: ASTStatement | undefined = undefined;
   if (this.peek()?.type === LexerTokenType.Alpha && this.peek().value === "else") {
     this.#tokens.shift(); // consume "else"
-    falseCase = this.parseBlockStatement(); // ✅ bloc {}
+    falseCase = this.parseBlockStatement(); 
   }
 
   return {
