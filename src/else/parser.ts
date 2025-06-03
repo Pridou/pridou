5eb0c923c4dc76acd709e0122b6ef80a44667ef7
matteo0003 <angoin.matteo@gmail.com>
2025-06-03 21:12:58 +0200

    refactor(parser): Refactored parser tokens list

---
+++ src/else/parser.ts
@@ -0,0 +1,31 @@
+import { LexerTokenType } from "@/types/lexer";
+
+export const MULTIPLICATIVE_OPERATORS: Set<string> = new Set([
+	LexerTokenType.Modulus,
+	LexerTokenType.Multiply,
+	LexerTokenType.Divide,
+]);
+
+export const ADDITIVE_OPERATORS: Set<string> = new Set([
+	LexerTokenType.Plus,
+	LexerTokenType.Minus,
+]);
+
+export const COMPARISON_OPERATORS: Set<string> = new Set([
+	LexerTokenType.LessThan,
+	LexerTokenType.LessThanOrEqual,
+	LexerTokenType.Equality,
+	LexerTokenType.GreaterThan,
+	LexerTokenType.GreaterThanOrEqual,
+]);
+
+export const ASSIGNMENT_OPERATORS: Set<string> = new Set([
+	LexerTokenType.Increment,
+	LexerTokenType.Decrement,
+	LexerTokenType.Assignment,
+	LexerTokenType.ModulusEquals,
+	LexerTokenType.MultiplyEquals,
+	LexerTokenType.PlusEquals,
+	LexerTokenType.MinusEquals,
+	LexerTokenType.DivideEquals,
+]);
